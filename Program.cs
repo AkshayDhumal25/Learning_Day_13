f920c4bdf0666177e1145757f3ea0e3f2d5b957c
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-04-01 11:11:04 +0530

    Find the reverse of the string.

--- Program.cs
+++ Program.cs
@@ -94,46 +94,64 @@
 
 
 // 3. Logic for anagram program with its time complexity. (for large strings).
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            string str1 = "listen";
+//            string str2 = "silemt";
+
+//            if (isAnagram(str1, str2))
+//            {
+//                Console.WriteLine("Anagram");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Not Anagram");
+//            }
+//        }
+//        public static bool isAnagram(string str1, string str2)
+//        {
+//            if (str1.Length != str2.Length)
+//            {
+//                return false;
+//            }
+//            int[] array = new int[256];
+//            for (int i = 0; i < str1.Length; i++)
+//            {
+//                array[str1[i]]++;
+//                array[str2[i]]--;
+//            }
+//            foreach (int i in array)
+//            {
+//                if (i != 0)
+//                {
+//                    return false;
+//                }
+//            }
+//            return true;
+
+//        }
+//    }
+//}
+
+// 4. Find the reverse of the string.
 using System;
+using System.Linq;
+using System.Collections.Generic;
 namespace logicals
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            string str1 = "listen";
-            string str2 = "silemt";
-
-            if (isAnagram(str1, str2))
-            {
-                Console.WriteLine("Anagram");
-            }
-            else
-            {
-                Console.WriteLine("Not Anagram");
-            }
-        }
-        public static bool isAnagram(string str1, string str2)
-        {
-            if (str1.Length != str2.Length)
-            {
-                return false;
-            }
-            int[] array = new int[256];
-            for (int i = 0; i < str1.Length; i++)
-            {
-                array[str1[i]]++;
-                array[str2[i]]--;
-            }
-            foreach (int i in array)
-            {
-                if (i != 0)
-                {
-                    return false;
-                }
-            }
-            return true;
-
+            string str = "Akshay";
+            var reversed = str.Reverse().ToArray();
+            reversed.ToString();
+            Console.WriteLine(reversed);
         }
     }
 }

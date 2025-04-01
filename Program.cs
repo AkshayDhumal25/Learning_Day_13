a100d7b3bddb95e87c2c61ee9126830b8b21a1d1
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-04-01 10:30:29 +0530

    find second largest element from the array;

--- Program.cs
+++ Program.cs
@@ -1,23 +1,51 @@
 ﻿//Find the missing number from the array
+//using System;
+//using System.Linq;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+//            int n = arr.Max();
+//            int realSum = n * (n + 1) / 2;
+//            int currentSum = 0;
+//            for (int i = 0; i < arr.Length; i++)
+//            {
+//                currentSum += arr[i];
+//            }
+
+//            Console.WriteLine(realSum - currentSum);
+
+//        }
+//    }
+//}
+
+// find second largest element from the array;
 using System;
-using System.Linq;
 namespace logicals
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            int n = arr.Max();
-            int realSum = n * (n + 1) / 2;
-            int currentSum = 0;
-            for (int i = 0; i < arr.Length; i++)
+            int[] array = { 1, 2, 4, 0, 3 };
+            int secLarge = 0;
+            int large = 0;
+            for (int i = 0; i < array.Length; i++)
             {
-                currentSum += arr[i];
+                if (array[i] > large)
+                {
+                    secLarge = large;
+                    large = array[i];
+                }
+                if (array[i] < large && array[i] > secLarge)
+                {
+                    secLarge = array[i];
+                }
             }
-
-            Console.WriteLine(realSum - currentSum);
-
+            Console.WriteLine(secLarge);
         }
     }
 }
